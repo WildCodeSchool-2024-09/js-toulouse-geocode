d947338c89f9503c3dec3a11fab6003ef5e99d2c
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2025-01-06 11:15:22 +0100

    fix: move logos to public/images

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -9,22 +9,22 @@ function MenuBar() {
         <ul>
           <li>
             <Link to="/">
-              <img src="./src/assets/images/home.svg" alt="home" />
+              <img src="/images/home.svg" alt="home" />
             </Link>
           </li>
           <li>
             <Link to="/map">
-              <img src="./src/assets/images/map.svg" alt="map" />
+              <img src="/images/map.svg" alt="map" />
             </Link>
           </li>
           <li>
             <Link to="/user">
-              <img src="./src/assets/images/user.svg" alt="user" />
+              <img src="/images/user.svg" alt="user" />
             </Link>
           </li>
           <li>
             <Link to="/contact">
-              <img src="./src/assets/images/contact.svg" alt="contact" />
+              <img src="/images/contact.svg" alt="contact" />
             </Link>
           </li>
         </ul>
