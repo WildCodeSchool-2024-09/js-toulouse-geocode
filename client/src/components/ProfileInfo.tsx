df775da0a74c74a9519391942b91146f026e0807
Gamberge <cedric.righi@gmail.com>
2025-01-09 15:46:23 +0100

    feat: create profil main page

--- client/src/components/ProfileInfo.tsx
+++ client/src/components/ProfileInfo.tsx
@@ -5,9 +5,9 @@ import ModifyIcon from "/images/modify.svg";
 export default function ProfileInfo() {
   return (
     <div className="profile-info-container">
-      <div className="profile-photo-container">
+      <section className="profile-photo-container">
         <img src={EmptyProfilePhoto} alt="default user profile" />
-        <div className="photo-butons">
+        <div className="photo-buttons">
           <button type="button">
             <img src={ModifyIcon} alt="pp-modify" />
           </button>
@@ -15,30 +15,28 @@ export default function ProfileInfo() {
             <img src={DeleteIcon} alt="pp-delete" />
           </button>
         </div>
-      </div>
-      <div className="personal-infos-container">
-        <div className="name-container">
+      </section>
+      <section className="personal-infos-container">
+        <article className="name-container">
           <p>Nom</p>
           <p>Prénom</p>
-        </div>
-        <div className="sex-and-birth-container">
+        </article>
+        <article className="sex-and-birth-container">
           <p>Sexe</p>
           <p>Date de naissance</p>
-        </div>
+        </article>
         <p>Mail</p>
-        <div className="location">
+        <article className="location">
           <p>Code postal</p>
           <p>Ville</p>
-        </div>
-      </div>
-      <div className="edit-profile-buttons">
+        </article>
         <button type="button" className="modify-profile-button">
           Modifier
         </button>
         <button type="button" className="delete-profile-button">
           Supprimer le profil
         </button>
-      </div>
+      </section>
     </div>
   );
 }
