6637862fe72cd8b14b39b99705e0e1bbc03ef099
Gamberge <cedric.righi@gmail.com>
2025-01-16 15:00:25 +0100

    feat: create vehicles list page

---
+++ client/src/components/VehiclesInfosCard.tsx
@@ -0,0 +1,30 @@
+interface VehiclesInfosCardProps {
+  vehicleBrand: string;
+  vehicleModel: string;
+  chargingVehicleType: string;
+}
+
+export default function VehiclesInfosCard({
+  vehicleBrand,
+  vehicleModel,
+  chargingVehicleType,
+}: VehiclesInfosCardProps) {
+  return (
+    <article className="vehicles-infos-card-container">
+      <div className="brand-and-model-car">
+        <div className="brand-car">
+          <h2>Marque</h2>
+          <p>{vehicleBrand}</p>
+        </div>
+        <div className="model-car">
+          <h2>Modèle</h2>
+          <p>{vehicleModel}</p>
+        </div>
+      </div>
+      <div className="charging-vehicle-type">
+        <h2>Type de charge</h2>
+        <p>{chargingVehicleType}</p>
+      </div>
+    </article>
+  );
+}
---
+++ client/src/components/VehiclesInfosList.tsx
@@ -0,0 +1,51 @@
+import VehiclesInfosCard from "./VehiclesInfosCard";
+
+export default function VehiclesInfo() {
+  const vehiclesInfos = [
+    {
+      id: 1,
+      vehicleBrand: "Audi",
+      vehicleModel: "RS3",
+      chargingVehicleType: "T2",
+    },
+    {
+      id: 2,
+      vehicleBrand: "Tesla",
+      vehicleModel: "Model 3",
+      chargingVehicleType: "T2",
+    },
+    {
+      id: 3,
+      vehicleBrand: "Renault",
+      vehicleModel: "Zoe",
+      chargingVehicleType: "T2",
+    },
+    {
+      id: 4,
+      vehicleBrand: "Peugeot",
+      vehicleModel: "208",
+      chargingVehicleType: "T2",
+    },
+  ];
+
+  return (
+    <section className="vehicles-info-container">
+      <article className="vehicles-infos-header">
+        <h1>Mes véhicules</h1>
+        <button type="button" className="add-vehicle-button">
+          Ajouter un véhicule
+        </button>
+      </article>
+      <article className="grid-car-infos-cards">
+        {vehiclesInfos.map((item) => (
+          <VehiclesInfosCard
+            key={item.id}
+            vehicleBrand={item.vehicleBrand}
+            vehicleModel={item.vehicleModel}
+            chargingVehicleType={item.chargingVehicleType}
+          />
+        ))}
+      </article>
+    </section>
+  );
+}
