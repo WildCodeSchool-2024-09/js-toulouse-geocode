7d6ce27dc2174bc53e77054e6a30265c1208c96e
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2025-01-16 17:20:13 +0100

    fix: correct responsiveness, flex display

--- client/src/components/StationTitle.tsx
+++ client/src/components/StationTitle.tsx
@@ -1,10 +1,14 @@
 import { useStationsLocationsContext } from "../contexts/StationsLocationsContextProvider";
 
 interface StationTitleProps {
+  stationCollapsed: boolean;
   setStationCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function StationTitle({ setStationCollapsed }: StationTitleProps) {
+function StationTitle({
+  stationCollapsed,
+  setStationCollapsed,
+}: StationTitleProps) {
   const handleClick = () => {
     setStationCollapsed((prev) => !prev);
   };
@@ -14,7 +18,9 @@ function StationTitle({ setStationCollapsed }: StationTitleProps) {
   const stationsLocationsContext = useStationsLocationsContext();
 
   return (
-    <div className="station-title">
+    <div
+      className={stationCollapsed ? "station-title collapsed" : "station-title"}
+    >
       <h2>{stationsLocationsContext.station.name}</h2>
       <img
         src="/images/arrow-collapse.svg"
