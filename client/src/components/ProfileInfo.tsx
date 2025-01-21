d43b2db8307e8ae9bccfdc9c62a5db37ceffb4b3
Gamberge <cedric.righi@gmail.com>
2025-01-21 10:40:21 +0100

    feat: create dialog box to modify infos

--- client/src/components/ProfileInfo.tsx
+++ client/src/components/ProfileInfo.tsx
@@ -3,16 +3,28 @@ import ArticlePersonalInfo from "./ArticlePersonalInfo";
 import ProfilePhoto from "./ProfilePhoto";
 
 import "../styles/ProfileInfo.css";
+import ModifyProfileInfos from "./ModifyProfileInfos";
 
 export default function ProfileInfo() {
-  const personalInfoList = [
-    ["Righi", "Cédric"],
-    ["Masculin", "27/03/2004"],
-    ["cedric.righi@gmail.com"],
-    ["09120", "Varilhes"],
+  const user = {
+    lastName: "Righi",
+    firstName: "Cédric",
+    sex: "Masculin",
+    birthday: "27/03/2004",
+    email: "cedric.righi@gmail.com",
+    postalcode: "09120",
+    city: "Varilhes",
+  };
+
+  const userArr = [
+    [user.lastName, user.firstName],
+    [user.sex, user.birthday],
+    [user.email],
+    [user.postalcode, user.city],
   ];
 
   const [photoFileUrl, setPhotoFileUrl] = useState<string | null>(null);
+  const [isModifyingProfile, setIsModifyingProfile] = useState(false);
 
   const fetchPhoto = useCallback(async () => {
     try {
@@ -37,31 +49,40 @@ export default function ProfileInfo() {
   }, [fetchPhoto]);
 
   return (
-    <div className="profile-info-container">
-      <ProfilePhoto
-        photoFileUrl={photoFileUrl}
-        setPhotoFileUrl={setPhotoFileUrl}
-        fetchPhoto={fetchPhoto}
-      />
-      <section className="personal-infos-container">
-        {personalInfoList.map((item) => (
-          <ArticlePersonalInfo
-            key={personalInfoList.indexOf(item)}
-            infos={item}
-          />
-        ))}
-        <div className="user-profile-buttons">
-          <button type="button" className="modify-profile-button">
-            Modifier
-          </button>
-          <button type="button" className="disconnect-profile-button">
-            Se déconnecter
-          </button>
-          <button type="button" className="delete-profile-button">
-            Supprimer le profil
-          </button>
-        </div>
-      </section>
-    </div>
+    <>
+      <div className="profile-info-container">
+        <ProfilePhoto
+          photoFileUrl={photoFileUrl}
+          setPhotoFileUrl={setPhotoFileUrl}
+          fetchPhoto={fetchPhoto}
+        />
+        <section className="personal-infos-container">
+          {userArr.map((item) => (
+            <ArticlePersonalInfo key={userArr.indexOf(item)} infos={item} />
+          ))}
+          <div className="user-profile-buttons">
+            <button
+              type="button"
+              className="modify-profile-button"
+              onClick={() => setIsModifyingProfile(true)}
+            >
+              Modifier
+            </button>
+            <button type="button" className="disconnect-profile-button">
+              Se déconnecter
+            </button>
+            <button type="button" className="delete-profile-button">
+              Supprimer le profil
+            </button>
+          </div>
+        </section>
+      </div>
+      {isModifyingProfile && (
+        <ModifyProfileInfos
+          setIsModifyingProfile={setIsModifyingProfile}
+          user={user}
+        />
+      )}
+    </>
   );
 }
