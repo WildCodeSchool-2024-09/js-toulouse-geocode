a6fe92c186e017ab5684821def7e17eb9c6a98f6
Gamberge <cedric.righi@gmail.com>
2025-01-28 11:11:59 +0100

    fix: fix conflicts

--- client/src/components/ModifyProfileInfos.tsx
+++ client/src/components/ModifyProfileInfos.tsx
@@ -182,7 +182,7 @@ export default function ModifyProfileInfos({
         <div className="modify-location-container" id="location">
           <input
             className="modify-postalcode-input"
-            type="text"
+            type="number"
             placeholder="Code postal"
             name="postalcode"
             value={postalcodeInput}
