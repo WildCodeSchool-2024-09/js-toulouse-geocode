89ba37530cabadbcbc2ea76b49206741ce483977
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2025-01-30 13:30:21 +0100

    feat: implement vehicle modification component

--- client/src/admin/pages/VehiclePage.tsx
+++ client/src/admin/pages/VehiclePage.tsx
@@ -1,14 +1,16 @@
 import ContentAdmin from "../components/ContentAdmin";
 import DeleteModal from "../components/DeleteModal";
 import HeaderAdminPage from "../components/HeaderAdminPage";
+import VehicleModification from "../components/VehicleModification";
 import { useModal } from "../contexts/ShowModalProvider";
 
 export default function VehiclePage() {
-  const { displayDeleteModal } = useModal();
+  const { displayModification, displayDeleteModal, itemId } = useModal();
   return (
-    <div className="station-page-container">
+    <div className="vehicle-page-container">
       <HeaderAdminPage title="Gestion des Vehicules" />
       <ContentAdmin titles={["Model", "Propriétaire"]} path="vehicles" />
+      {displayModification && <VehicleModification vehicleId={itemId} />}
       {displayDeleteModal && (
         <DeleteModal
           title="du véhicule"
