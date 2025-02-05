13690999f12bf72532ebe3e173f45f86a2580dad
Hakkenheiner <remi.zicken@gmail.com>
2025-02-05 14:37:10 +0100

    style: add style to upload page

--- client/src/admin/pages/UploadSationsPage.tsx
+++ client/src/admin/pages/UploadSationsPage.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DragAndDrop from "../components/DragAndDrop";
 import HeaderAdminPage from "../components/HeaderAdminPage";
+import "../styles/UploadSationsPage.css";
+import { useShowNav } from "../contexts/ShowNavProvider";
 
 export default function UploadSationsPage() {
   const [file, setFile] = useState<File | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
+  const { setNavVisible } = useShowNav();
 
   const updateDatabase = async (file: File) => {
     const formData = new FormData();
@@ -26,19 +30,38 @@ export default function UploadSationsPage() {
       );
     }
   };
+
+  useEffect(() => {
+    setNavVisible(true);
+  }, [setNavVisible]);
+
   return (
     <div className="upload-page-container">
       <HeaderAdminPage title="Mise a jour des bornes" />
       <div className="upload-page-drap-and-drop-container">
         <h2>Upload file</h2>
-        <DragAndDrop setFile={setFile} />
-        <p>
-          Format supporté : <strong>CSV</strong>
-        </p>
+        <DragAndDrop setFile={setFile} setMessage={setMessage} />
+        <section>
+          <p>
+            Format supporté : <strong>CSV</strong>
+          </p>
+          <p
+            className={
+              message === "Le fichier n'est pas de type CSV"
+                ? "message-error"
+                : ""
+            }
+          >
+            {message}
+          </p>
+        </section>
         {file && (
-          <div>
-            <p>{file.name}</p>
-            <button type="button" onClick={() => updateDatabase(file)}>
+          <div className="upload-page-button-container">
+            <button
+              className="upload-page-button"
+              type="button"
+              onClick={() => updateDatabase(file)}
+            >
               Mettre à jour
             </button>
           </div>
